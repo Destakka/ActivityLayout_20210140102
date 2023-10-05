9103517ab3dac2657403c82c234c85f96026fdc1
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-10-05 07:02:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pam_act1/framelayout.kt
@@ -0,0 +1,11 @@
+package com.example.pam_act1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class framelayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_framelayout)
+    }
+}
